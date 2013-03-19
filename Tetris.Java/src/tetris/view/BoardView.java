596f1c3c2f9224814c5ce67ac65f5809d4b23bf2
Timothy Prepscius <tprepscius>
2013-03-19 14:11:48 -0400

    added a copyright and license description

--- Tetris.Java/src/tetris/view/BoardView.java
+++ Tetris.Java/src/tetris/view/BoardView.java
@@ -1,3 +1,13 @@
+///////////////////////////////////////////////////////////////////////////////
+// Copyright (C) 2013 Timothy Prepscius
+//
+// This is a viewable source license.  No copying or modification or use is
+// permitted.
+//
+// If you would like to participate in its development, or
+// if you would like a different license, please contact the author.
+///////////////////////////////////////////////////////////////////////////////
+
 package tetris.view;
 
 import tetris.model.Board;
